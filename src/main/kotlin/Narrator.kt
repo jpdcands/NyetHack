c5623d82e1944223086fde9e5f6df21739dc528b
Paul <jpdcands211@gmail.com>
2022-11-11 16:41:43 -0500

    pg. 149

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,8 +1,9 @@
 fun narrate(
     message: String
 ) {
-    println({
+    val narrationModifier: () -> String = {
         val numExclamationPoints = 3
         message.uppercase() + "!".repeat(numExclamationPoints)
-    }())
+    }
+    println(narrationModifier())
 }
